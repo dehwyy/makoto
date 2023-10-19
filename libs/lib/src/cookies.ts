efe10244179a468154d6c2db971443643d5379e1
dehwyy <dehwyy@yandex.ru>
2023-10-19 10:10:33 +0300

    @makoto/lib/cookies (1, 2)
    1. MakotoCookiesInterface instead of Cookies from `@sveltejs/kit` (I don't know why I didn't notice that earlier)
    2. Remove MakotoCookiesAut(h)orization (it actually was `MakotoCookiesAutorization` w/o `h` bruh) for unnecessarity

--- libs/lib/src/cookies.ts
+++ libs/lib/src/cookies.ts
@@ -1,20 +1,23 @@
-import type { RpcMetadata } from '@protobuf-ts/runtime-rpc/build/types/rpc-metadata'
-import type { Cookies as CookiesType } from '@sveltejs/kit'
+interface CookieSetOptions {
+	httpOnly?: boolean
+	path?: string
+}
+
+export interface MakotoCookiesInterface {
+	get(name: string): string | undefined;
+	set(name: string, value: string, opts?: CookieSetOptions): void;
+	delete(name: string): void;
+}
 
 export class MakotoCookies {
-	static setGlobal(cookies: CookiesType, key: string, value: string) {
+	static setGlobal(cookies: MakotoCookiesInterface, key: string, value: string) {
 		cookies.set(key, value, {
 			path: '/',
 			httpOnly: true
 		})
 	}
-}
 
-export class MakotoCookiesAutorization {
-	static setToken(headers: RpcMetadata, cookies: CookiesType) {
-		const split_token = (headers['authorization'] as string).split(' ')
-		if (split_token.length < 2) return
-
-		MakotoCookies.setGlobal(cookies, 'token', split_token[1])
+	static delete(cookies: MakotoCookiesInterface, key: string) {
+		cookies.delete(key)
 	}
 }
