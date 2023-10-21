221f146eb8a45034f27a24bd1f43c7ca8a8d1290
dehwyy <dehwyy@yandex.ru>
2023-10-21 09:27:40 +0300

    @makoto/lib/typed-fetch add typed return values

--- libs/lib/src/typed-fetch.ts
+++ libs/lib/src/typed-fetch.ts
@@ -1,18 +1,29 @@
 export class TypedFetch {
   // create a typed fetch
-  static Create = <T>(url: string, method = 'POST') =>
-	(data: T) => {
-		return fetch(url, {
+  static Create = <T, R = void>(url: string, method = 'POST') =>
+	async (data: T) => {
+		const response = await fetch(url, {
 			method: method,
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(data)
 		})
+
+		let response_data: R | underfined = undefined
+		if (!response.ok) {
+			response_data = await response.json()
+		}
+
+
+		return {
+			...response,
+			data: response_data
+		}
 	}
 
   // read body of request (on server side f.e.)
   static Get = async <T extends (...args: any) => any>(req: Request, fn: T) => {
-	return (await req.json()) as Awaited<Parameters<typeof fn>[0]>
+		return (await req.json()) as Awaited<Parameters<typeof fn>[0]>
   }
 }
