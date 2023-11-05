5a4b3dcc2fcb79ac0d77edcab3ad25ba40fb5855
dehwyy <dehwyy@yandex.ru>
2023-11-05 10:54:38 +0300

    changed grpc.generator (1, 2), changed ts imports (migrate to native ts -> js via @protobuf-ts)
    1. Generated files depends on ENV provided: TWIRP_GO_COMPILE, TWIRP_TS_COMPILE;
    2. Instead of generating all files it would iter through Vec of `protos_name` which should be implicitly set
    
    t
    
    t
    
    t
    
    t
    
    t
    
    t
    
    fixed workflow

--- libs/grpc/lib/clients/hashmap.ts
+++ libs/grpc/lib/clients/hashmap.ts
@@ -1,5 +1,5 @@
 import { TwirpFetchTransport } from '@protobuf-ts/twirp-transport'
-import { HashmapRPCClient as GeneratedHashmapClient } from "../../generated/hashmap/hashmap.client"
+import { HashmapRPCClient as GeneratedHashmapClient } from "@makoto/grpc/generated/hashmap"
 
 const transport = new TwirpFetchTransport({
 	baseUrl: (process.env.TWIRP_GATEWAY_URL || "http://localhost:4000") + "/hashmap",
