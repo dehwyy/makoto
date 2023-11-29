85ff1cd1671b2c2e7c50c16b1d3dff2ff3cff333
dehwyy <dehwyy@yandex.ru>
2023-11-29 23:45:34 +0300

    rewrite config for rust

--- libs/config/src/lib.rs
+++ libs/config/src/lib.rs
@@ -2,10 +2,6 @@ use envconfig::Envconfig;
 use dotenv;
 use std::env::current_dir as cwd;
 
-pub trait ConfigRead {
-    fn new() -> Self;
-}
-
 #[derive(Envconfig, Debug, Default)]
 pub struct Config {
     #[envconfig(from = "USER_URL")]
@@ -13,13 +9,10 @@ pub struct Config {
 
     #[envconfig(from = "DATABASE_URL")]
     pub database_url: String,
-
-    #[envconfig(from = "REDIS_BASE_URL", default = "redis://127.0.0.1:6379")]
-    pub redis_base_url: String
 }
 
-impl ConfigRead for Config {
-    fn new() -> Self {
+impl Config {
+    pub fn new() -> Self {
         // load env;
         let env_path = cwd().unwrap().join(".env");
         let _ = dotenv::from_path(env_path);
@@ -34,34 +27,14 @@ impl ConfigRead for Config {
 }
 
 pub mod constants {
-    pub use super::*;
-
-    #[derive(Envconfig, Debug, Default)]
-    pub struct Nats {
-        // ? Servers
-        #[envconfig(from = "NATS_SERVER_DEFAULT", default = "localhost:4222")]
-        pub server_default: String,
-
-        // ? jetstreams
-        #[envconfig(from = "NATS_JETSTREAM_SERVICE_DISCOVERY", default = "ServiceDiscovery")]
-        pub js_service_discovery: String,
-
-        // ? consumers
-        #[envconfig(from = "NATS_CONSUMER_DISCOVERY", default = "DiscoveryConsumer")]
-        pub consumer_discovery: String,
+    pub mod nats {
+        pub const SERVER_DEFAULT: &str = "localhost:4222";
+        pub const JS_SERVICE_DISCOVERY: &str = "ServiceDiscovery";
+        pub const CONSUMER_DISCOVERY: &str = "DiscoveryConsumer";
     }
 
-    impl ConfigRead for Nats {
-        fn new() -> Self {
-            let env_path = cwd().unwrap().join(".env");
-            let _ = dotenv::from_path(env_path);
-
-            match Self::init_from_env() {
-                Ok(val) => val,
-                Err(_) => Nats {
-                    ..Default::default()
-                }
-            }
-        }
+    pub mod redis {
+        pub const SERVER_DEFAULT: &str = "redis://127.0.0.1:6379";
+        pub const HASHMAP_KEY_SERVICES: &str = "services";
     }
 }
